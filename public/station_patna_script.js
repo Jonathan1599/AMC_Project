97a5e97f2f697ada002ced95d542780638834c90
Jonathan D'mello <jonathandmello@yahoo.co.in>
2021-12-08 16:22:28 +0530

    fix : add changes to patna station

--- public/station_patna_script.js
+++ public/station_patna_script.js
@@ -33,50 +33,60 @@ var chart1 = new Chart(ctx, {
  
 });
 
+let threshold = 0;
+const err = document.getElementById("error");
+const statuss = document.getElementById("status");
+
+const thresh_btn = document.getElementById("thresh-btn");
+thresh_btn.addEventListener("click", () => {
+  t = document.getElementById("threshold").value;
+  console.log(t);
+  threshold = t;
+})
+
+
+
+
+function detectEarthQuake(x){
+  let count = 0;
+  x.forEach(element => {
+    if(element > threshold)
+      count = count + 1;
+  });
+
+  if(count > 10){
+    statuss.innerText = "EarthQuake detected!"
+    console.log("Earthquake")
+  }
+
+  else{
+    statuss.innerText = ""
+    console.log("no Earthquake")
+    console.log(count)
+  }
+
+}
+
 let socket = io.connect("http://3.130.18.167:9000");
 
 socket.on("patna", (data) => {
   console.log(data);
-  if(labels.length > 100){
+  if(labels.length > 150){
     labels.shift();
     x.shift();
     y.shift();
     z.shift();
+    detectEarthQuake(x);
+    detectEarthQuake(y);
   }
+if(data.z < data.x || data.z < data.y)
+  err.innerText = "Check sensor configuration"
+else
+  err.innerText = ""
+
   x.push(data.x);
   y.push(data.y);
   z.push(data.z);
   labels.push(labels[labels.length - 1] + 1);
-  chart1.update();
+  chart1.update(); 
 });
-
-// const ctx = document.getElementById('myChart');
-// const myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: ['Red', 'Blue',  'Green'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// })
